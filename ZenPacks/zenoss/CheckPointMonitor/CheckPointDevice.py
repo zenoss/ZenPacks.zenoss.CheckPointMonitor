546b4208fa0171cdae3e40b56fabab547d99aa2f
Chet Luther <chet.luther@gmail.com>
2014-05-19 11:40:09 -0500

    Add support for Gaia OS.
    Fixes ZEN-11064.

--- ZenPacks/zenoss/CheckPointMonitor/CheckPointDevice.py
+++ ZenPacks/zenoss/CheckPointMonitor/CheckPointDevice.py
@@ -1,148 +1,152 @@
 ##############################################################################
-# 
-# Copyright (C) Zenoss, Inc. 2008, all rights reserved.
-# 
+#
+# Copyright (C) Zenoss, Inc. 2008, 2014, all rights reserved.
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
+from copy import deepcopy
 
 from Globals import InitializeClass
-from Products.ZenRelations.RelSchema import *
+
 from Products.ZenModel.Device import Device
 from Products.ZenModel.ZenossSecurity import ZEN_VIEW
-from copy import deepcopy
 
 
 class CheckPointDevice(Device):
     "A Check Point firewall"
-    
+
     fwModuleState = ""
-    
+
     fwVerMajor = None
     fwVerMinor = None
     fwPolicyName = ""
     fwInstallTime = ""
-    
+
     cpvVerMajor = None
     cpvVerMinor = None
-    
+
     haInstalled = None
     haVerMajor = None
     haVerMinor = None
     haStarted = None
     haState = ""
-    
+
+    memTotalReal = None
+    memTotalVirtual = None
+
     svnVersion = ""
-    
+
     osName = ""
+    osMajorVer = None
+    osMinorVer = None
     osVersionLevel = ""
-    
+
     dtpsVerMajor = None
     dtpsVerMinor = None
     dtpsLicensedUsers = None
-    
+
     _properties = Device._properties + (
-        {'id':'fwModuleState', 'type':'string', 'mode':'w'},
+        {'id': 'fwModuleState', 'type': 'string', 'mode': 'w'},
 
-        {'id':'fwVerMajor', 'type':'string', 'mode':'w'},
-        {'id':'fwVerMinor', 'type':'string', 'mode':'w'},
-        {'id':'fwPolicyName', 'type':'string', 'mode':'w'},
-        {'id':'fwInstallTime', 'type':'string', 'mode':'w'},
+        {'id': 'fwVerMajor', 'type': 'string', 'mode': 'w'},
+        {'id': 'fwVerMinor', 'type': 'string', 'mode': 'w'},
+        {'id': 'fwPolicyName', 'type': 'string', 'mode': 'w'},
+        {'id': 'fwInstallTime', 'type': 'string', 'mode': 'w'},
 
-        {'id':'cpvVerMajor', 'type':'string', 'mode':'w'},
-        {'id':'cpvVerMinor', 'type':'string', 'mode':'w'},
+        {'id': 'cpvVerMajor', 'type': 'string', 'mode': 'w'},
+        {'id': 'cpvVerMinor', 'type': 'string', 'mode': 'w'},
 
-        {'id':'haInstalled', 'type':'string', 'mode':'w'},
-        {'id':'haVerMajor', 'type':'string', 'mode':'w'},
-        {'id':'haVerMinor', 'type':'string', 'mode':'w'},
-        {'id':'haStarted', 'type':'string', 'mode':'w'},
-        {'id':'haState', 'type':'string', 'mode':'w'},
+        {'id': 'haInstalled', 'type': 'string', 'mode': 'w'},
+        {'id': 'haVerMajor', 'type': 'string', 'mode': 'w'},
+        {'id': 'haVerMinor', 'type': 'string', 'mode': 'w'},
+        {'id': 'haStarted', 'type': 'string', 'mode': 'w'},
+        {'id': 'haState', 'type': 'string', 'mode': 'w'},
 
-        {'id':'svnVersion', 'type':'string', 'mode':'w'},
+        {'id': 'memTotalReal', 'type': 'int', 'mode': 'w'},
+        {'id': 'memTotalVirtual', 'type': 'int', 'mode': 'w'},
 
-        {'id':'osName', 'type':'string', 'mode':'w'},
-        {'id':'osVersionLevel', 'type':'string', 'mode':'w'},
+        {'id': 'svnVersion', 'type': 'string', 'mode': 'w'},
 
-        {'id':'dtpsVerMajor', 'type':'string', 'mode':'w'},
-        {'id':'dtpsVerMinor', 'type':'string', 'mode':'w'},
-        {'id':'dtpsLicensedUsers', 'type':'string', 'mode':'w'},
+        {'id': 'osName', 'type': 'string', 'mode': 'w'},
+        {'id': 'osMajorVer', 'type': 'int', 'mode': 'w'},
+        {'id': 'osMinorVer', 'type': 'int', 'mode': 'w'},
+        {'id': 'osVersionLevel', 'type': 'string', 'mode': 'w'},
+
+        {'id': 'dtpsVerMajor', 'type': 'string', 'mode': 'w'},
+        {'id': 'dtpsVerMinor', 'type': 'string', 'mode': 'w'},
+        {'id': 'dtpsLicensedUsers', 'type': 'string', 'mode': 'w'},
         )
-    
-    
+
     factory_type_information = deepcopy(Device.factory_type_information)
     custom_actions = []
     custom_actions.extend(factory_type_information[0]['actions'])
-    custom_actions.insert(2,
-           { 'id'              : 'checkPointDeviceDetail'
-           , 'name'            : 'Check Point'
-           , 'action'          : 'checkPointDeviceDetail'
-           , 'permissions'     : (ZEN_VIEW, ) },
-           )
-    factory_type_information[0]['actions'] = custom_actions
-    
-        
-    def __init__(self, *args, **kw):
-        Device.__init__(self, *args, **kw)
-        self.buildRelations()
+    custom_actions.insert(2, {
+        'id': 'checkPointDeviceDetail',
+        'name': 'Check Point Details',
+        'action': 'checkPointDeviceDetail',
+        'permissions': (ZEN_VIEW, ),
+        })
 
+    factory_type_information[0]['actions'] = custom_actions
 
     def getOsVersionString(self):
         if self.osName and self.osVersionLevel:
             return "%s (%s)" % (self.osName, self.osVersionLevel)
         return "Unknown"
-    
+
     def getSvnVersionString(self):
         return self.svnVersion or "Unknown"
-    
+
     def getFwVersionString(self):
         if self.fwVerMajor is not None and self.fwVerMinor is not None:
             return "%s.%s" % (self.fwVerMajor, self.fwVerMinor)
         return "Unknown"
-    
+
     def getCpvVersionString(self):
         if self.cpvVerMajor is not None and self.cpvVerMinor is not None:
             return "%s.%s" % (self.cpvVerMajor, self.cpvVerMinor)
         return "Unknown"
-    
+
     def getHaVersionString(self):
         if self.haVerMajor is not None and self.haVerMinor is not None:
             return "%s.%s" % (self.haVerMajor, self.haVerMinor)
         return "Unknown"
-    
+
     def getDtpsVersionString(self):
         if self.dtpsVerMajor is not None and self.dtpsVerMinor is not None:
             return "%s.%s" % (self.dtpsVerMajor, self.dtpsVerMinor)
         return "Unknown"
-    
+
     def getFwPolicyName(self):
         return self.fwPolicyName or "Unknown"
-    
+
     def getFwInstallTime(self):
         return self.fwInstallTime or "Unknown"
-    
+
     def getFwStatus(self):
         return self.getStatus(where="component = 'firewall'")
-    
+
     def getHaInstalled(self):
         if self.haInstalled:
             return True
         return False
-    
+
     def getHaStarted(self):
         if self.haStarted and self.haStarted == "yes":
             return True
         return False
-    
+
     def getHaStatus(self):
         return self.getStatus(where="component = 'HA'")
-    
+
     def getDtpsLicensedUsers(self):
         if self.dtpsLicensedUsers is not None:
             return self.dtpsLicensedUsers
         return "Unknown"
-    
+
     def getDtpsStatus(self):
         return self.getStatus(where="component = 'policy server'")
 
--- ZenPacks/zenoss/CheckPointMonitor/modeler/plugins/CheckPoint/DeviceMap.py
+++ ZenPacks/zenoss/CheckPointMonitor/modeler/plugins/CheckPoint/DeviceMap.py
@@ -1,68 +1,77 @@
 ##############################################################################
-# 
-# Copyright (C) Zenoss, Inc. 2008, all rights reserved.
-# 
+#
+# Copyright (C) Zenoss, Inc. 2008, 2014, all rights reserved.
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 
-__doc__ = """DeviceMap
-Maps device level information from Trango access points
+"""Models Check Point device information using SNMP.
+
+Uses CHECKPOINT-MIB.
+
 """
 
 from Products.DataCollector.plugins.CollectorPlugin import SnmpPlugin, GetMap
 from Products.DataCollector.plugins.DataMaps import ObjectMap, MultiArgs
 
+
 class DeviceMap(SnmpPlugin):
-    """Maps device level information from CheckPoint firewalls
-    """
-        
+
+    """Check Point device modeler plugin."""
+
     snmpGetMap = GetMap({
         '.1.3.6.1.4.1.2620.1.1.1.0': 'fwModuleState',
-        
+
         '.1.3.6.1.4.1.2620.1.1.22.0': 'fwVerMajor',
         '.1.3.6.1.4.1.2620.1.1.23.0': 'fwVerMinor',
         '.1.3.6.1.4.1.2620.1.1.25.1.0': 'fwPolicyName',
         '.1.3.6.1.4.1.2620.1.1.25.2.0': 'fwInstallTime',
-        
+
         '.1.3.6.1.4.1.2620.1.2.2.0': 'cpvVerMajor',
         '.1.3.6.1.4.1.2620.1.2.3.0': 'cpvVerMinor',
-        
+
         '.1.3.6.1.4.1.2620.1.5.2.0': 'haInstalled',
         '.1.3.6.1.4.1.2620.1.5.3.0': 'haVerMajor',
         '.1.3.6.1.4.1.2620.1.5.4.0': 'haVerMinor',
         '.1.3.6.1.4.1.2620.1.5.5.0': 'haStarted',
         '.1.3.6.1.4.1.2620.1.5.6.0': 'haState',
-        
-        '.1.3.6.1.4.1.2620.1.6.4.1.0': 'svnVersion', 
-        
+
+        '.1.3.6.1.4.1.2620.1.6.4.1.0': 'svnVersion',
+
         '.1.3.6.1.4.1.2620.1.6.5.1.0': 'osName',
+        '.1.3.6.1.4.1.2620.1.6.5.2.0': 'osMajorVer',
+        '.1.3.6.1.4.1.2620.1.6.5.3.0': 'osMinorVer',
         '.1.3.6.1.4.1.2620.1.6.5.7.0': 'osVersionLevel',
-        
+
         '.1.3.6.1.4.1.2620.1.6.7.1.1.0': 'memTotalVirtual',
         '.1.3.6.1.4.1.2620.1.6.7.1.3.0': 'memTotalReal',
-        
+        '.1.3.6.1.4.1.2620.1.6.7.4.1.0': '_memTotalVirtual64',
+        '.1.3.6.1.4.1.2620.1.6.7.4.3.0': '_memTotalReal64',
+
         '.1.3.6.1.4.1.2620.1.9.2.0': 'dtpsVerMajor',
         '.1.3.6.1.4.1.2620.1.9.3.0': 'dtpsVerMinor',
         '.1.3.6.1.4.1.2620.1.9.4.0': 'dtpsLicensedUsers',
         })
-    
-    
+
     def process(self, device, results, log):
-        """collect snmp information from this device"""
+        """Return DataMaps given device and results."""
         log.info('processing %s for device %s', self.name(), device.id)
         getdata, tabledata = results
-        if getdata['fwModuleState'] is None: return None
+
+        if getdata.get('fwModuleState') is None:
+            return None
+
         maps = []
         om = self.objectMap(getdata)
-        
+
         # Handle all of the OS possibilities.
-        if "SecurePlatform" in om.osName:
+        if "SecurePlatform" in om.osName or "Gaia" in om.osName:
             manufacturer = "Check Point"
         elif "Nokia" in om.osName:
-            manufacuturer = "Nokia"
+            manufacturer = "Nokia"
         elif "Linux" in om.osName:
             manufacturer = "Linux"
         elif "Solaris" in om.osName:
@@ -71,10 +80,31 @@ class DeviceMap(SnmpPlugin):
             manufacturer = "Microsoft"
         else:
             manufacturer = "Unknown"
-        
+
+        if not om.osVersionLevel.strip():
+            major = getdata.get('osMajorVer')
+            minor = getdata.get('osMinorVer')
+            if major is not None and minor is not None:
+                om.osVersionLevel = '{}.{}'.format(major, minor)
+
         om.setOSProductKey = MultiArgs(
-            "%s (%s)" % (om.osName, om.osVersionLevel), manufacturer)
+            "%s (%s)" % (om.osName, om.osVersionLevel),
+            manufacturer)
+
+        # Prefer 64bit version of memTotalReal if it exists.
+        try:
+            om.memTotalReal = int(getdata['_memTotalReal64'])
+        except Exception:
+            pass
+
+        # Prefer 64bit version of memTotalVirtual if it exists.
+        try:
+            om.memTotalVirtual = int(getdata['_memTotalVirtual64'])
+        except Exception:
+            pass
+
         maps.append(om)
         maps.append(ObjectMap({"totalMemory": om.memTotalReal}, compname="hw"))
         maps.append(ObjectMap({"totalSwap": om.memTotalVirtual}, compname="os"))
+
         return maps
