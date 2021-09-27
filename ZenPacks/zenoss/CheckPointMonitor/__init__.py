19a34dfc3f38235f69e1c72da4af55c8ca32eaf5
mkotovska <mkotovska@zenoss.com>
2021-09-27 14:53:15 +0300

    SVC-2986: move manufacturer to __init__; add path to skin dir

--- ZenPacks/zenoss/CheckPointMonitor/__init__.py
+++ ZenPacks/zenoss/CheckPointMonitor/__init__.py
@@ -8,6 +8,10 @@
 ##############################################################################
 
 import os
+import logging
+
+from Products.CMFCore.DirectoryView import registerDirectory
+from Products.Zuul.facades.manufacturersfacade import ManufacturersFacade
 
 from ZenPacks.zenoss.ZenPackLib import zenpacklib
 
@@ -15,9 +19,43 @@ from ZenPacks.zenoss.ZenPackLib import zenpacklib
 CFG = zenpacklib.load_yaml([os.path.join(os.path.dirname(__file__), "zenpack.yaml")], verbose=False, level=30)
 schema = CFG.zenpack_module.schema
 
+skinsDir = os.path.join(os.path.dirname(__file__), 'skins')
+if os.path.isdir(skinsDir):
+    registerDirectory(skinsDir, globals())
+
+log = logging.getLogger("zen.CheckPoint")
+
+
+MANUFACTURER_NAME = 'Check Point'
+MANUFACTURER_UID = '/zport/dmd/Manufacturers/Check Point'
+
 
 class ZenPack(schema.ZenPack):
 
     def install(self, app):
         super(ZenPack, self).install(app)
-        ## TODO move manufacturer from objects.xml
+
+        # create 'Check Point' manufacturer and its products if they don't exist yet
+        manufacturers = app.dmd.Manufacturers
+        if MANUFACTURER_NAME not in manufacturers:
+            mFacade = ManufacturersFacade(manufacturers)
+            mFacade.addManufacturer(MANUFACTURER_NAME)
+            log.info('New manufacturer [{}] was added'.format(MANUFACTURER_NAME))
+
+            mFacade.addNewProduct({
+                'uid': MANUFACTURER_UID,
+                'prodname': 'Gaia (2.6)',
+                'prodkeys': 'Gaia (2.6)',
+                'type': 'Operating System',
+                'partno': '',
+                'description': ''
+            })
+
+            mFacade.addNewProduct({
+                'uid': MANUFACTURER_UID,
+                'prodname': 'SecurePlatform (NGX R65)',
+                'prodkeys': 'SecurePlatform (NGX R65)',
+                'type': 'Hardware',
+                'partno': '',
+                'description': ''
+            })
